e0e293761e410b0c23da0df3ac316f44984a0a55
JaffaBakess <104676626+JaffaBakess@users.noreply.github.com>
2022-06-27 13:48:21 +0100

    Create button.js

---
+++ button.js
@@ -0,0 +1,10 @@
+var clicks = 0;
+function clickME() {
+  clicks += 1;
+  document.getElementById("clicks").innerHTML = clicks;
+}
+var clicker = 0;
+function clickerME() {
+  clicks += 1;
+  document.getElementById("clicker").innerHTML = clicks;
+}
